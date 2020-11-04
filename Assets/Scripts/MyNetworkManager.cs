6fd873d07b1697abdaaba927fc6e69d9af9236d2
Akash Athreya <47862571+AkashAthreya@users.noreply.github.com>
2020-11-04 21:23:01 +0530

    Network manager 1

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnStartServer()
+    {
+        Debug.Log("server started");
+
+    }
+    public override void OnStopServer()
+    {
+        Debug.Log("server sstopped");
+
+    }
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log("Connected to server!");
+
+    }
+    public override void OnClientDisconnect(NetworkConnection conn)
+    {
+        Debug.Log("Disconected from server!");
+    }
+}
